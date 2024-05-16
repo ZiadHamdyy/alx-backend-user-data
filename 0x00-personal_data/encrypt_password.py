4be4b3d66dca8ffc65c458b02daf97d56c6dacc3
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-16 23:30:05 +0300

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,3 +13,10 @@ def hash_password(password: str) -> bytes:
     # Hash the password with the generated salt
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
