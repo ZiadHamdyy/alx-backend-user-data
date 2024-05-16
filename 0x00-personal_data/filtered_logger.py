35a2d191ca79b9617fe60f1375d17d2ba3c88f75
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-16 22:34:20 +0300

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """filter_datum returns the log message obfuscated"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """filter_datum returns the log message obfuscated"""
     return re.sub(rf'({"|".join(fields)})=[^{separator}]*',
                   lambda m: f"{m.group().split('=')[0]}={redaction}", message)
