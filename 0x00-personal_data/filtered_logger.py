f99628fe0b2b0d247e1dd5ed5dc56c115d67525c
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-16 23:20:58 +0300

    task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """filter_datum returns the log message obfuscated"""
-import re
-from typing import List, Tuple
 import logging
-
+import re
+import os
+from typing import List, Tuple
+import mysql.connector
+from mysql.connector.connection import MySQLConnection
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
@@ -50,3 +52,23 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> MySQLConnection:
+    """
+    Connects to the MySQL database using credentials from environment variables.
+
+    Returns:
+        mysql.connector.connection.MySQLConnection: The database connection object.
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
