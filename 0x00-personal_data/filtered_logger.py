6e7ef8b74f7f3d082c049db8d78862473222525c
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-16 22:46:26 +0300

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """filter_datum returns the log message obfuscated"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -9,3 +10,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """filter_datum returns the log message obfuscated"""
     return re.sub(rf'({"|".join(fields)})=[^{separator}]*',
                   lambda m: f"{m.group().split('=')[0]}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize the formatter with specified fields to redact."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the log record, redacting specified fields."""
+        original_message = super(RedactingFormatter, self).format(record)
+        redacted_message = filter_datum(self.fields, self.REDACTION,
+                                        original_message, self.SEPARATOR)
+        return redacted_message
