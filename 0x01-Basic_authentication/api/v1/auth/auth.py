ef12afa03aabd28ebf59883cf2b923b9bb45ada3
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-21 21:01:28 +0300

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+class to manage the API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """class to manage the API authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method require_auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """public method authorization_header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """public method current_user"""
+        return None
