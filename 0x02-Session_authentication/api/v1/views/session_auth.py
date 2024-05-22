1d4c14d436b99f60964df6a2803eb0be7866b7b8
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-22 22:46:56 +0300

    task 8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -43,6 +43,7 @@ def login():
 @app_views.route('/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
 def logout():
+    """logout route"""
     if not auth.destroy_session(request):
         abort(404)
 
