c8384655447e5a1d1572e2f1815c3fe554c89e43
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-22 22:17:04 +0300

    task 6

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -28,6 +28,7 @@ else:
 
 excluded_paths = ['/api/v1/status/', '/api/v1/auth_session/login/']
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -57,8 +58,9 @@ def before_request_func():
 
     if not auth.require_auth(request.path, excluded_paths):
         return None
-    
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+
+    if auth.authorization_header(request)\
+            is None and auth.session_cookie(request) is None:
         abort(401)
 
     request.current_user = auth.current_user(request)
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """SessionAuth class that inherits from Auth."""
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -29,3 +30,17 @@ class SessionAuth(Auth):
 
         user_id = self.__class__.user_id_by_session_id.get(session_id)
         return user_id
+
+    def current_user(self, request=None):
+        """
+        Retrieves the User instance based on a cookie value.
+        """
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return None
+
+        return User.get(user_id)
