4aece73321384d44469e101f9089c76452949ec0
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-22 22:46:13 +0300

    task 8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -6,6 +6,7 @@ from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models.user import User
 from os import getenv
+from api.v1.app import auth
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -37,3 +38,12 @@ def login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
