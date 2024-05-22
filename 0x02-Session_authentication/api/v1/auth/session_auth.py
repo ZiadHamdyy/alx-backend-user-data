9c0ab03bd3d88ff9f02f4e93fe530f66b9768f8b
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-22 22:31:37 +0300

    task 7

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,10 @@
 """SessionAuth class that inherits from Auth."""
 from api.v1.auth.auth import Auth
 import uuid
+from flask import jsonify, abort, request, make_response
+from api.v1.views import app_views
 from models.user import User
+from os import getenv
 
 
 class SessionAuth(Auth):
@@ -44,3 +47,33 @@ class SessionAuth(Auth):
             return None
 
         return User.get(user_id)
+    
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get('password')
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({'email': email})
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = users[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    if not session_id:
+        abort(500)
+
+    response = make_response(user.to_json())
+    session_name = getenv('SESSION_NAME')
+    response.set_cookie(session_name, session_id)
+
+    return response
