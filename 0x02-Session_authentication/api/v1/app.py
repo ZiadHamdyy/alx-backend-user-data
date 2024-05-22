602f2aeb1fdaccb79005a1363ba16718642c1261
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-22 21:53:19 +0300

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -26,6 +26,7 @@ elif AUTH_TYPE == "session_auth":  # Add this condition
 else:
     auth = Auth()
 
+excluded_paths = ['/api/v1/status/', '/api/v1/auth_session/login/']
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -54,12 +55,10 @@ def before_request_func():
     if auth is None:
         return
 
-    exempt_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                    '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, exempt_paths):
-        return
-
-    if auth.authorization_header(request) is None:
+    if not auth.require_auth(request.path, excluded_paths):
+        return None
+    
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
 
     request.current_user = auth.current_user(request)
